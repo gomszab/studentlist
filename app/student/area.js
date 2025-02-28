5c8f5ddfc237f2ab70674b83ea1fc0f8e863c7a2
gomszab <gombosszabolcs88@gmail.com>
2025-02-28 21:56:36 +0100

    refactor areas add new functions for callbacks

--- app/student/area.js
+++ app/student/area.js
@@ -1,11 +1,17 @@
 class Area{
     #div;
+    #manager;
+
+    get manager(){
+        return this.#manager;
+    }
 
     get div(){
         return this.#div;
     }
 
-    constructor(className){
+    constructor(className, manager){
+        this.#manager = manager;
         const container = this.#getContainer();
         this.#div = document.createElement('div');
         this.#div.className = className;
@@ -26,8 +32,12 @@ class Area{
 class StudentArea extends Area{
 
     constructor(className, manager){
-        super(className);
-        manager.setAddCallback((student) => {
+        super(className, manager);
+        this.manager.setAddCallback(this.#addCallback())
+    }
+
+    #addCallback(){
+        return (student) => {
             const studentCard = document.createElement('div');
             studentCard.className = 'student-card';
             const span = document.createElement('span');
@@ -40,28 +50,36 @@ class StudentArea extends Area{
             averageSpan.textContent = student.average;
             studentCard.appendChild(averageSpan);
 
-            studentCard.addEventListener('click', (e) => {
-                const cardList = document.querySelectorAll('.student-card');
-                for(const card of cardList){
-                    card.className = 'student-card';
-                }
-                e.currentTarget.classList.add('selected');
-                manager.select(student);
-            })
+            studentCard.addEventListener('click',this.#onClickStudentCard(student))
             this.div.appendChild(studentCard);
 
-        })
+        }
+    }
+
+    #onClickStudentCard(student){
+        return (e) => {
+            const cardList = document.querySelectorAll('.student-card');
+            for(const card of cardList){
+                card.className = 'student-card';
+            }
+            e.currentTarget.classList.add('selected');
+            this.manager.select(student);
+        }
     }
 }
 
 class DetailsArea extends Area{
     constructor(className, manager){
-        super(className);
-        manager.setSelectCallback((student) => {
+        super(className, manager);
+        manager.setSelectCallback(this.#selectCallback())
+    }
+
+    #selectCallback(){
+        return (student) => {
             this.div.innerHTML = '';
             const detailContainer = document.createElement('div');
             detailContainer.innerHTML = student.comment;
             this.div.appendChild(detailContainer);
-        })
+        }
     }
 }
